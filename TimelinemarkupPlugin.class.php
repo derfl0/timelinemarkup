d481e3a5368881e95d56b5a5ea9fd308af7a646d
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-12-02 12:13:29 +0100

    fix height and width

--- TimelinemarkupPlugin.class.php
+++ TimelinemarkupPlugin.class.php
@@ -1,19 +1,18 @@
 <?php
 
 class TimelinemarkupPlugin extends StudIPPlugin implements SystemPlugin {
-
+    
     public function __construct() {
         parent::__construct();
         StudipFormat::addStudipMarkup('timeline', '\[timeline\]', '\[\/timeline\]', 'TimelinemarkupPlugin::Timeline');
-        PageLayout::addScript($this->getPluginURL() . '/assets/timeline.js');
         PageLayout::addScript("https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['timeline']}]}");
+        PageLayout::addScript($this->getPluginURL() . '/assets/timeline.js');
     }
 
     public static function Timeline($markup, $matches, $contents) {
 
         // Create id
         $id = uniqid('timeline');
-
 
         preg_match_all("/\((\w|,|.|:)*\)/", $contents, $inputs);
 
@@ -22,16 +21,16 @@ class TimelinemarkupPlugin extends StudIPPlugin implements SystemPlugin {
             $data = explode(',', $entry);
             $data[1] = self::jsonDate($data[1]);
             $data[2] = self::jsonDate($data[2]);
-            
+
             $rows[] = "['{$data[0]}', {$data[1]}, {$data[2]}]";
         }
-        
-       $container = "<div style='width: 300px; height: 180px;' id='$id' class='timeline'>[".join(',',$rows)."]</div><script>var $id = [".join(',',$rows)."]</script>";
+
+        $container = "<div style='height: 300px;' id='$id' class='timeline'>[" . join(',', $rows) . "]</div><script>var $id = [" . join(',', $rows) . "]</script>";
         return $container;
     }
-    
+
     public static function jsonDate($input) {
-        return "new Date(".date("Y, m, d", strtotime($input)).")";
+        return "new Date(" . date("Y, m, d", strtotime($input)) . ")";
     }
 
 }
